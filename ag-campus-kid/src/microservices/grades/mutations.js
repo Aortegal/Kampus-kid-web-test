9b0dad56d6d9f713dcb811f97891e83ea2195e2b
YB-SM75 <42952888+yybeltranr@users.noreply.github.com>
2022-11-07 19:41:38 -0500

    AG funcional

--- ag-campus-kid/src/microservices/grades/mutations.js
+++ ag-campus-kid/src/microservices/grades/mutations.js
@@ -14,4 +14,74 @@ module.exports = {
   deleteGrades: async (root, {id}) => {
     return await HttpProvider.deleted(`${baseUrl}/app/grades/${id}`);
   },
+
+  createStudents: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/students`, data);
+  },
+  updateStudents: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/students/${id}`);
+  },
+  deleteStudents: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/students/${id}`);
+  },
+
+  createTeachers: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/teachers`, data);
+  },
+  updateTeachers: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/teachers/${id}`);
+  },
+  deleteTeachers: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/teachers/${id}`);
+  },
+
+  createSchedules: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/schedules`, data);
+  },
+  updateSchedules: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/schedules/${id}`);
+  },
+  deleteSchedules: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/schedules/${id}`);
+  },
+
+  createPeriods: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/periods`, data);
+  },
+  updatePeriods: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/periods/${id}`);
+  },
+  deletePeriods: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/periods/${id}`);
+  },
+
+  createGroups: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/groups`, data);
+  },
+  updateGroups: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/groups/${id}`);
+  },
+  deleteGroups: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/groups/${id}`);
+  },
+
+  createEnrollments: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/enrollments`, data);
+  },
+  updateEnrollments: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/enrollments/${id}`);
+  },
+  deleteEnrollments: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/enrollments/${id}`);
+  },
+
+  createSubjects: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/app/subjects`, data);
+  },
+  updateSubjects: async (root, {id}) => {
+    return await HttpProvider.put(`${baseUrl}/app/subjects/${id}`);
+  },
+  deleteSubjects: async (root, {id}) => {
+    return await HttpProvider.deleted(`${baseUrl}/app/subjects/${id}`);
+  },
 };
--- ag-campus-kid/src/microservices/grades/queries.js
+++ ag-campus-kid/src/microservices/grades/queries.js
@@ -11,4 +11,53 @@ module.exports = {
   getGradeById: async (id) => {
     return await HttpProvider.get(`${baseUrl}/app/grades`+id);
   },
+
+  getStudents: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/students`);
+  },
+  getStudentById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/students`+id);
+  },
+
+  getTeachers: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/teachers`);
+  },
+  getTeacherById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/teachers`+id);
+  },
+
+  getSchedules: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/schedules`);
+  },
+  getScheduleById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/schedules`+id);
+  },
+
+  getPeriods: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/periods`);
+  },
+  getPeriodById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/periods`+id);
+  },
+
+  getGroups: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/groups`);
+  },
+  getGroupById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/groups`+id);
+  },
+
+  getEnrollments: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/enrollments`);
+  },
+  getEnrollmentById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/enrollments`+id);
+  },
+
+  getSubjects: async () => {
+    return await HttpProvider.get(`${baseUrl}/app/subjects`);
+  },
+  getSubjectById: async (id) => {
+    return await HttpProvider.get(`${baseUrl}/app/subjects`+id);
+  },
 };
