95870c84d39c9ad274887c57d1e6ec72d2af933e
adol2299 <adol2299@gmail.com>
2022-11-19 23:30:07 -0500

    async login

--- appweb-campus-kid/src/components/modalLogin.js
+++ appweb-campus-kid/src/components/modalLogin.js
@@ -28,25 +28,25 @@ export const FormikFormStudentsLogin = () => {
     function handleSubmit(e) {
         e.preventDefault()
         var { username, password } = e.target.elements
-        console.log({ username: username.value, password: password.value })
         setUsuario(username.value);
         setContrasena(password.value);
         mutacion();
 
-        if(login != undefined) {
-            if(login.roles[0] == "ROLE_MODERATOR"){
-                window.location=`/teacher`
-            }else if(login.roles[0] == "ROLE_USER"){
-                window.location=`/student`
+        console.log(login);
+        if (login != undefined) {
+            if (login.roles[0] == "ROLE_MODERATOR") {
+                window.location = `/teacher`
+            } else if (login.roles[0] == "ROLE_USER") {
+                window.location = `/student`
             }
-        }else{
+        } else {
             alert("Check credentials, login invalid");
         }
 
 
     }
 
-    function mutacion() {
+    async function mutacion() {
         const GET_LOGIN = `
         mutation signin_prueba{
             login(data: { username: "${usuario}" , password:"${contrasena}" }) {
@@ -57,24 +57,27 @@ export const FormikFormStudentsLogin = () => {
         }
     }
     `
-        fetch("http://localhost:5000/api", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ query: GET_LOGIN })
-        }).then(response => response.json())
-            .then(data => setLogin(data.data.login))
-            
-            
+        const respuesta = await fetch("http://localhost:5000/api", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ query: GET_LOGIN })
+            })
+                .then(response => response.json())
+                .then(data => setLogin(data.data.login))
+
+        console.log(login);
+
+        respuesta();    
     }
 
-    
+
 
 
     return (
         <>
-            <div className="container" style={{ display: 'flex'}}>
-                <div className="col 8" style={{ display: 'flex', flexDirection: "column", textAlign: "center",marginTop: "110px",  justifyContent: "space-between"  }}>
-                    <div style={{display: 'flex', flexDirection: "column", textAlign: "center", justifyContent: "center", padding: "10px 45px 10px 45px"}}>
+            <div className="container" style={{ display: 'flex' }}>
+                <div className="col 8" style={{ display: 'flex', flexDirection: "column", textAlign: "center", marginTop: "110px", justifyContent: "space-between" }}>
+                    <div style={{ display: 'flex', flexDirection: "column", textAlign: "center", justifyContent: "center", padding: "10px 45px 10px 45px" }}>
                         <h3 style={{ textAlign: "center" }}>Login</h3><br /><br />
                         <form onSubmit={handleSubmit} >
                             <div className="form-group">
@@ -86,11 +89,11 @@ export const FormikFormStudentsLogin = () => {
                             <button className="btn btn-lg button-submit" type="submit" value="Submit" style={{ textAlign: "center" }}> Submit</button>
                         </form>
                     </div>
-                    <div style={{borderTop: "solid 2px", margin: "0px -10px 0px -32px", paddingTop:"15PX"}}>
+                    <div style={{ borderTop: "solid 2px", margin: "0px -10px 0px -32px", paddingTop: "15PX" }}>
                         <p>Don't have an account ? <b>Sign up now</b></p>
                     </div>
                 </div>
-                <div className="col 4" style={{ display: 'flex', backgroundColor: "#D9D9D9", marginRight: "-24px", justifyContent: "center", borderLeft:"solid 2px" }}>
+                <div className="col 4" style={{ display: 'flex', backgroundColor: "#D9D9D9", marginRight: "-24px", justifyContent: "center", borderLeft: "solid 2px" }}>
                     <img className="img-fluid" src="/images/group 14login.png" style={{ marginBottom: "60px", maxWidth: "60%" }} />
                 </div>
             </div>
